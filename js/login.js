96ae1dcbdfbd677ec53548f3c3adb3a2357ad3df
Guilherme Lins <guilhermelins978@gmail.com>
2022-08-26 17:17:52 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,10 +1,23 @@
-function confirmar() {
-    const email = document.querySelector("input#email").value
-    const senha = document.querySelector("input#senha").value
-    const emailFake = "email"
-    const senhaFake = "senha"
-    console.log({email,senha})
-    if (email == emailFake && senha == senhaFake) {
-    window.location.href="index.html"
+const email = document.querySelector("input#email").value
+const senha = document.querySelector("input#senha")
+const olho = document.querySelector("img#olho")
+ 
+function clickOlho() {
+    let inputTypeIsPassword = senha.type == "password"
+
+    if (inputTypeIsPassword) {
+        mostarSenha()
+    } else {
+        ocultarSenha()
     }
+}
+
+function mostarSenha() {
+    senha.setAttribute("type", "text")
+    olho.setAttribute("src", "olho-aberto.png")
+}
+
+function ocultarSenha() {
+    senha.setAttribute("type", "password")
+    olho.setAttribute("src", "olho-fechado.png")
 }
