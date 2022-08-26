cccf834d2e4d12021e48c2cea33f64573f1fcd60
Guilherme Lins <guilhermelins978@gmail.com>
2022-08-26 17:24:41 -0300

    Create criarLogin.js

---
+++ js/criarLogin.js
@@ -0,0 +1,23 @@
+const email = document.querySelector("input#email").value
+const senha = document.querySelector("input.senha")
+const olho = document.querySelector("img.olho")
+ 
+function clickOlho() {
+    let inputTypeIsPassword = senha.type == "password"
+
+    if (inputTypeIsPassword) {
+        mostarSenha()
+    } else {
+        ocultarSenha()
+    }
+}
+
+function mostarSenha() {
+    senha.setAttribute("type", "text")
+    olho.setAttribute("src", "olho-aberto.png")
+}
+
+function ocultarSenha() {
+    senha.setAttribute("type", "password")
+    olho.setAttribute("src", "olho-fechado.png")
+}
