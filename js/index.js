1b57b99978c9684edd642a4819c62d3a79fced7d
Guilherme Lins <guilhermelins978@gmail.com>
2023-03-25 15:59:47 -0300

    Recriando o design do login e criar login

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
+document.querySelector('.botao_agendamento').addEventListener('click', function () {
+    location = '../html/atendimento.html'
+});
+
+document.querySelector('#btn_login').addEventListener('click', function () {
+    location = '../html/login.html'
+});
+
+document.querySelector('#btn_criar_login').addEventListener('click', function () {
+    location = '../html/criarLogin.html'
+});
 
 
